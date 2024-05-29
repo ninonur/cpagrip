e14a802150d1820357b27a07f85e5e3ac92cdc89
ninonur <39723243+ninonur@users.noreply.github.com>
2024-05-29 10:49:41 +0900

    Update cpa-go.js

--- remote/cpa-go.js
+++ remote/cpa-go.js
@@ -1,1 +1,20 @@
 
+var json = 'https://blossombabes.my.id/cpagrip/jsoncpagrip.php';
+
+var xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", json, true);
+xhttp.responseType = "json"
+xhttp.send();
+xhttp.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    var arr = this.response.offers;
+    var item = arr[0];
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i].payout > item.payout) {
+        item = arr[i];
+      }
+    }
+    console.log(item);
+  }
+};
